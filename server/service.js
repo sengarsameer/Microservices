23c337bbde963dc5d6ea49c425f3feff98f3efb9
sengarsameer <sengar.sameer@gmail.com>
2020-01-24 22:23:03 +0530

    registering services

--- server/service.js
+++ server/service.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const ServiceRegistry = require('./lib/serviceRegistry')
 const service = express();
 // const ServiceRegistry = require('./ServiceRegistry');
 
 module.exports = (config) => {
   const log = config.log();
+  const serviceRegistry = new ServiceRegistry(log);
   // Add a request logging middleware in development mode
   if (service.get('env') === 'development') {
     service.use((req, res, next) => {
@@ -13,8 +14,11 @@ module.exports = (config) => {
     });
   }
 
-  service.put('/register/:servicename/:serviceversion/:serviceport', (req, res, next) => {
-    return next('Not implemented');
+  service.put('/register/:servicename/:serviceversion/:serviceport', (req, res) => {
+    const {servicename, serviceversion, serviceport} = req.params;
+    const serviceip = req.connection.remoteAddress.includes('::') ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+    const serviceKey = serviceRegistry.register(servicename, serviceversion, serviceport);
+    return res.json({result, serviceKey});
   });
 
   service.delete('/register/:servicename/:serviceversion/:serviceport', (req, res, next) => {
